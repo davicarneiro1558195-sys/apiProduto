a727be72c7ece70d48212e8f5579b4bc09040206
davicarneiro1558195-sys <davi.carneiro.1558195@sga.pucminas.br>
2026-08-21 18:31:12 -0300

    Resolvendo defeitos da api.

--- app.js
+++ app.js
@@ -5,40 +5,40 @@ app.use(express.json())
 
 const produtos = [
     {
-    "id": 1,
-    "descricao": "Teclado Mecânico",
-    "preco": 249.90,
-    "categoria": "Periféricos",
-    "estoque": 15
-  },
-  {
-    "id": 2,
-    "descricao": "Mouse Gamer",
-    "preco": 129.90,
-    "categoria": "Periféricos",
-    "estoque": 30
-  },
-  {
-    "id": 3,
-    "descricao": "Headset Gamer",
-    "preco": 199.90,
-    "categoria": "Áudio",
-    "estoque": 20
-  },
-  {
-    "id": 4,
-    "descricao": "Monitor 24 Polegadas",
-    "preco": 899.90,
-    "categoria": "Monitores",
-    "estoque": 10
-  },
-  {
-    "id": 5,
-    "descricao": "Webcam Full HD",
-    "preco": 159.90,
-    "categoria": "Acessórios",
-    "estoque": 25
-  }
+        "id": 1,
+        "descricao": "Teclado Mecânico",
+        "preco": 249.90,
+        "categoria": "Periféricos",
+        "estoque": 15
+    },
+    {
+        "id": 2,
+        "descricao": "Mouse Gamer",
+        "preco": 129.90,
+        "categoria": "Periféricos",
+        "estoque": 30
+    },
+    {
+        "id": 3,
+        "descricao": "Headset Gamer",
+        "preco": 199.90,
+        "categoria": "Áudio",
+        "estoque": 20
+    },
+    {
+        "id": 4,
+        "descricao": "Monitor 24 Polegadas",
+        "preco": 899.90,
+        "categoria": "Monitores",
+        "estoque": 10
+    },
+    {
+        "id": 5,
+        "descricao": "Webcam Full HD",
+        "preco": 159.90,
+        "categoria": "Acessórios",
+        "estoque": 25
+    }
 ]
 
 app.get('/produtos', (req, res) => {
@@ -46,12 +46,12 @@ app.get('/produtos', (req, res) => {
 })
 
 app.get('/produtos/:id', (req, res) => {
-    
+
     const id = parseInt(req.params.id)
 
     const produto = produtos.find(produto => produto.id === id)
-    
-    
+
+
     if (produto) {
         res.json(produto)
     } else {
@@ -62,22 +62,19 @@ app.get('/produtos/:id', (req, res) => {
 
 app.post('/produtos', (req, res) => {
 
-    const produto = req.body
-
-    const existe = produtos.find(p => p.id === produto.id)
-
     const maiorId = Math.max(...produtos.map(p => p.id))
 
-    produto.id = maiorId + 1
-
-    if (existe) {
-        res.status(400)
-        res.send("Esse ID já existe!")
-    } else {
-        produtos.push(produto)
-        res.status(201)
-        res.json(produto)
+    const produto = {
+        id: maiorId + 1,
+        ...req.body
     }
+
+
+    produtos.push(produto)
+    res.status(201)
+    console.log("Created")
+    res.json(produto)
+
 })
 
 app.put('/produtos/:id', (req, res) => {
@@ -88,7 +85,10 @@ app.put('/produtos/:id', (req, res) => {
 
     if (index != -1) {
 
-        produtos[index] = req.body
+        produtos[index] = {
+            id: id,
+            ...req.body
+        }
 
         res.json(produtos[index])
 
@@ -99,16 +99,17 @@ app.put('/produtos/:id', (req, res) => {
 })
 
 app.delete('/produtos/:id', (req, res) => {
-    
+
     const id = parseInt(req.params.id)
 
     const index = produtos.findIndex(produto => produto.id === id)
-    
-    
+
+
     if (index != -1) {
         res.status(204)
-        produtos.splice(index,1)
+        produtos.splice(index, 1)
         res.json(produtos)
+        res.send("Produto deletado!")
     } else {
         res.status(404)
         res.send("Produto não encontrado!")
@@ -118,3 +119,11 @@ app.delete('/produtos/:id', (req, res) => {
 app.listen(process.env.PORT || 3000, '0.0.0.0', () => {
     console.log('Servidor funcionando')
 })
+
+
+/* 
+
+    app.listen(3000, (e) => {
+    console.log('Servidor ouvindo em http://localhost:3000')
+})
+*/
